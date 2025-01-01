af835737d2e6b9b9e3bae904b2fd191738aa3005
maoshan-classmate <452309154@qq.com>
2025-01-01 15:53:15 +0800

    修改

--- src/main/java/com/ms/aspect/MsLimitCheckAspect.java
+++ src/main/java/com/ms/aspect/MsLimitCheckAspect.java
@@ -1,6 +1,7 @@
 package com.ms.aspect;
 
 import cn.hutool.core.annotation.AnnotationUtil;
+import cn.hutool.core.text.CharSequenceUtil;
 import cn.hutool.core.util.ReflectUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.extra.servlet.ServletUtil;
@@ -37,25 +38,25 @@ public class MsLimitCheckAspect {
     private ApplicationContext applicationContext;
 
 
-    Map<Integer, Cache<String, AtomicInteger>> cacheMap = Maps.newHashMap();
+    Map<String, Cache<String, AtomicInteger>> cacheMap = Maps.newHashMap();
 
     /**
      * 初始化缓存Map
      */
-    @PostConstruct
-    public void init() {
-        for (String beanDefinitionName : applicationContext.getBeanDefinitionNames()) {
-            Object bean = applicationContext.getBean(beanDefinitionName);
-            for (Method method : ReflectUtil.getMethods(bean.getClass())) {
-                MsLimitCheck msLimitCheck = AnnotationUtil.getAnnotation(method, MsLimitCheck.class);
-                if (msLimitCheck == null || cacheMap.get(msLimitCheck.expireCache()) != null) {
-                    continue;
-                }
-                Cache<String, AtomicInteger> limitCache = CacheBuilder.newBuilder().expireAfterAccess(msLimitCheck.expireCache(), msLimitCheck.timeUnit()).maximumSize(1000).build();
-                cacheMap.put(msLimitCheck.expireCache(), limitCache);
-            }
-        }
-    }
+//    @PostConstruct
+//    public void init() {
+//        for (String beanDefinitionName : applicationContext.getBeanDefinitionNames()) {
+//            Object bean = applicationContext.getBean(beanDefinitionName);
+//            for (Method method : ReflectUtil.getMethods(bean.getClass())) {
+//                MsLimitCheck msLimitCheck = AnnotationUtil.getAnnotation(method, MsLimitCheck.class);
+//                if (msLimitCheck == null || cacheMap.get(method.getName()+":"+msLimitCheck.expireCache()) != null) {
+//                    continue;
+//                }
+//                Cache<String, AtomicInteger> limitCache = CacheBuilder.newBuilder().expireAfterAccess(msLimitCheck.expireCache(), msLimitCheck.timeUnit()).maximumSize(1000).build();
+//                cacheMap.put(method.getName()+":"+msLimitCheck.expireCache(), limitCache);
+//            }
+//        }
+//    }
 
     @Around("@annotation(com.ms.annotation.MsLimitCheck)")
     public Object process(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
@@ -66,9 +67,9 @@ public class MsLimitCheckAspect {
         if (msLimitCheck == null) {
             return proceedingJoinPoint.proceed();
         }
-        Cache<String, AtomicInteger> localCache = cacheMap.get(msLimitCheck.expireCache());
+        Cache<String, AtomicInteger> localCache = cacheMap.get(method.getName()+":"+msLimitCheck.expireCache());
         String lockKey = msLimitCheck.lockKey();
-        if (StrUtil.isEmpty(lockKey)) {
+        if (CharSequenceUtil.isEmpty(lockKey)) {
             ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
             if (requestAttributes != null) {
                 HttpServletRequest request = requestAttributes.getRequest();
@@ -80,7 +81,7 @@ public class MsLimitCheckAspect {
         // 若缓存不存在新增一个缓存
         if (localCache == null){
             localCache= CacheBuilder.newBuilder().expireAfterAccess(msLimitCheck.expireCache(), msLimitCheck.timeUnit()).maximumSize(1000).build();
-            cacheMap.put(msLimitCheck.expireCache(), localCache);
+            cacheMap.put(method.getName()+":"+msLimitCheck.expireCache(), localCache);
         }
         AtomicInteger atomicInteger = localCache.get(lockKey, () -> new AtomicInteger(1));
         atomicInteger.incrementAndGet();
