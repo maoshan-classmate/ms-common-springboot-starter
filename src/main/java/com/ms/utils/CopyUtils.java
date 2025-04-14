c8127b72fa085586760f4a2d9002fd470f31b1cb
xuzhengwei <xuzhengwei@ci2c.com.cn>
2025-04-14 15:05:35 +0800

    json字符串转Java对象(支持复杂类型)

--- src/main/java/com/ms/utils/CopyUtils.java
+++ src/main/java/com/ms/utils/CopyUtils.java
@@ -1,27 +1,36 @@
 package com.ms.utils;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.text.CharSequenceUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 /**
  * @author maoshan-classmate
- * @date 2024/9/19 10:55
  * @version 1.0
+ * @date 2024/9/19 10:55
  */
 public class CopyUtils extends BeanUtil {
 
     /**
      * 复制集合
+     *
      * @param sources 原集合
-     * @param target 目标对象
+     * @param target  目标对象
+     * @param <S>     原对象类型
+     * @param <T>     目标对象类型
      * @return 复制后的集合
-     * @param <S> 原对象类型
-     * @param <T> 目标对象类型
      */
     public static <S, T> List<T> copyListProperties(List<S> sources, Supplier<T> target) {
         return copyListProperties(sources, target, null);
@@ -29,12 +38,13 @@ public class CopyUtils extends BeanUtil {
 
     /**
      * 复制集合并进行拓展
-     * @param sources 原集合
-     * @param target 目标对象
+     *
+     * @param sources  原集合
+     * @param target   目标对象
      * @param callBack 回调
+     * @param <S>      原对象类型
+     * @param <T>      目标对象类型
      * @return 复制后的集合
-     * @param <S> 原对象类型
-     * @param <T> 目标对象类型
      */
     public static <S, T> List<T> copyListProperties(List<S> sources, Supplier<T> target, BiConsumer<S, T> callBack) {
         List<T> list = new ArrayList<>(sources.size());
@@ -52,13 +62,14 @@ public class CopyUtils extends BeanUtil {
 
     /**
      * 根据条件复制集合并进行拓展
-     * @param sources 原集合
-     * @param target 目标对象
+     *
+     * @param sources   原集合
+     * @param target    目标对象
      * @param condition 复制条件
-     * @param callBack 回调
+     * @param callBack  回调
+     * @param <S>       原对象类型
+     * @param <T>       目标对象类型
      * @return 复制后的集合
-     * @param <S> 原对象类型
-     * @param <T> 目标对象类型
      */
     public static <S, T> List<T> copyListProperties(List<S> sources, Supplier<T> target, Predicate<S> condition, BiConsumer<S, T> callBack) {
         List<T> list = new ArrayList<>(sources.size());
@@ -73,5 +84,54 @@ public class CopyUtils extends BeanUtil {
             }
         }
         return list;
+    }
+
+    /**
+     * 将json转换为Java对象(支持复杂类型)
+     *
+     * @param json    json字符串
+     * @param typeRef 要转化的Java对象的类型
+     * @param <T>     要转化的Java对象的类型
+     * @return Java对象
+     */
+    public static <T> T jsonToBean(String json, TypeReference<T> typeRef) {
+        if (CharSequenceUtil.isBlank(json)) {
+            return getDefaultInstance(typeRef);
+        }
+        try {
+            return JSON.parseObject(json, typeRef);
+        } catch (Exception e) {
+            throw new RuntimeException("json转换异常", e);
+        }
+    }
+
+    /**
+     * 根据传入的类型返回空集合或空对象
+     *
+     * @param typeRef 传入类型
+     * @param <T>     传入类型
+     * @return 返回空集合或空对象
+     */
+    private static <T> T getDefaultInstance(TypeReference<T> typeRef) {
+        Type type = typeRef.getType();
+        Class<?> rawType;
+        if (type instanceof Class) {
+            rawType = (Class<?>) type;
+        } else if (type instanceof ParameterizedType) {
+            rawType = (Class<?>) ((ParameterizedType) type).getRawType();
+        } else {
+            throw new IllegalArgumentException("不支持的类型: " + type);
+        }
+        if (Collection.class.isAssignableFrom(rawType)) {
+            return (T) Collections.emptyList();
+        } else if (Map.class.isAssignableFrom(rawType)) {
+            return (T) Collections.emptyMap();
+        } else {
+            try {
+                return (T) rawType.getDeclaredConstructor().newInstance();
+            } catch (ReflectiveOperationException e) {
+                throw new RuntimeException("无法创建默认实例: " + rawType.getName(), e);
+            }
+        }
     }
 }
